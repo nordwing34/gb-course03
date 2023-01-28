01aea37a1d5b4de46ea86bcd62df76399f138a71
Влад Нордвинг <vlad@nordwing.ru>
2023-01-28 14:42:49 +0300

    Work with task 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Привет, мир");
+﻿//Console.WriteLine("Привет, мир");
 
 // Задание 1
 // Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
@@ -7,4 +7,19 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
+
+string aStr = Console.ReadLine();
+string bStr = Console.ReadLine();
+
+int a = Convert.ToInt32(aStr);
+int b = Convert.ToInt32(bStr);
+int max = 0;
+
+if(a > b)
+	max = a;
+else
+	max = b;
+
+Console.Write("Максимальное число: " + max);
+
 
